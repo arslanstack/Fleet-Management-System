a4078d2da61046f1278925b32432faea98d4f86d
Amir <amirch357@gmail.com>
2023-09-21 19:13:49 +0500

    changing in frontend

--- fms-frontend/src/_helpers/auth-header.js
+++
@@ -1,9 +0,0 @@
-export function authHeader() {
-    // return authorization header with jwt token
-    let token = JSON.parse(localStorage.getItem('token'));
-    if (token) {
-        return { 'Authorization': token, 'secret': 'onemanarmy', 'Content-Type': 'application/json' };
-    } else {
-        return {};
-    }
-}
